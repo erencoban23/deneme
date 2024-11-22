0f3c52f15bd1a9ce67fc799389a4d86d4e2539f1
erencoban23 <156335663+erencoban23@users.noreply.github.com>
2024-11-23 00:21:22 +0300

    Add files via upload

---
+++ live.sh
@@ -0,0 +1,1 @@
+ffmpeg -stream_loop -1 -re -i video.mp4 -stream_loop -1 -re -i https://streams.ilovemusic.de/iloveradio3.mp3 -vcodec libx264 -pix_fmt yuvj420p -maxrate 20048k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150000k -f flv rtmp://live.restream.io/live/re_5949360_e4d5eff9b516c636b400
